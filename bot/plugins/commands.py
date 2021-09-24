666926aab2336c5791a3b6ab4ff22a73ec7b3571
movma <88189789+movma@users.noreply.github.com>
2021-09-24 11:26:28 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @MovieMania0710 ,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '❌JOIN OUR GROUP❌', url="https://t.me/MovieMania0710"
                                 )
                         ]
                     ]
